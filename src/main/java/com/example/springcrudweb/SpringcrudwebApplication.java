c5de0c3c1e9695b3da1d053e83bcc93c8222c11f
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-18 20:14:43 +0700

    create

---
+++ src/main/java/com/example/springcrudweb/SpringcrudwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcrudweb/SpringcrudwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrudweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
